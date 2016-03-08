5341a549f05e920024f08d201f8352b7d604cad0
shigemk2 <i.am.shige@gmail.com>
2016-03-09 02:25:34 +0900

    Parse

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -6,6 +6,7 @@ import org.json4s.native.JsonMethods._
 object Hello {
   def main(args: Array[String]): Unit = {
     println(parse(""" { "numbers": [ 1, 2, 3, 4 ] }"""))
+    println(parse("""{"name":"Toy","price":35.35}""", useBigDecimalForDouble = true))
     println("Hello, world!")
   }
 }
