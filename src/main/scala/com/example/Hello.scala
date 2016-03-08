e982e3daa3995a2ca628a2a159e448362c76adff
shigemk2 <i.am.shige@gmail.com>
2016-03-09 02:26:07 +0900

    Add comment

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -5,7 +5,9 @@ import org.json4s.native.JsonMethods._
 
 object Hello {
   def main(args: Array[String]): Unit = {
+    // JObject(List((numbers,JArray(List(JInt(1), JInt(2), JInt(3), JInt(4))))))
     println(parse(""" { "numbers": [ 1, 2, 3, 4 ] }"""))
+    // JObject(List((name,JString(Toy)), (price,JDecimal(35.35))))
     println(parse("""{"name":"Toy","price":35.35}""", useBigDecimalForDouble = true))
     println("Hello, world!")
   }
